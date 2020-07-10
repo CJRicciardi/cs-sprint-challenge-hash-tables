596bcd9774e4993036089935a57d419fd845708c
CJ Ricciardi <ricciardistg@gmail.com>
2020-07-10 17:58:47 -0400

    Ex 1 and Ex2 complete.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,53 +3,24 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-    cache = {}
-
-    done = False
-    index = 1
-    answer = None
-    # count = 1
-
-    # return print(reverse)
-
-
-    # if len(weights) == 1:
-    #     if weights[0] <= limit:
-    #         answer = []
-    #         answer.append(weights[0])
-    #         return answer
-    #     else:
-    #         return answer
-    if len(weights) <= 1:
-        return answer
-    else:
-        answer = []
-        def test(x, y):
-            z = None
-            # count = 0
-            for i in range(len(x)):
-                for j in range(len(x)):
-                    if i != j:
-                        cache[(i, j)] = x[i] + x[j]
-                        z = x[i] + x[j]
-                        if y == cache[(i, j)]:
-                            return i, j
+    
+    d = {}
+    def inner(weights, length, limit):
+        for i in range(length):
+            for j in range(length):
+                if i != j:
+                    if (weights[i]+weights[j]) not in d:
+                        d[weights[i]+weights[j]] = [i, j]
+                    elif limit in d:
+                        return d[limit]
                     else:
-                        pass           
+                        pass
+    
+    x = inner(weights, length, limit)
 
-        i, j = test(weights, limit)
-
-        numbers = []
-
-        if weights[i] > weights[j]:
-            numbers.append(weights[j])
-            numbers.append(weights[i])
-        else:
-            numbers.append(weights[i])
-            numbers.append(weights[j])
-
-        first = numbers[0]
-        second = numbers[1]
-        answer.append(weights.index(first))
-        answer.append(weights.index(second))
-        return answer
+    if length < 2:
+        return None
+    elif x[0] < x[1]:
+        x.reverse()
+    
+    return x
--- hashtables/ex1/trial.py
+++ hashtables/ex1/trial.py
@@ -1,6 +1,36 @@
-from ex1 import get_indices_of_item_weights
+# # from ex1 import get_indices_of_item_weights
 
 iterations = 1
+
+def get_indices_of_item_weights(weights, length, limit):
+    """
+    YOUR CODE HERE
+    """
+    # Your code here
+    
+    d = {}
+    def inner(weights, length, limit):
+        for i in range(length):
+            for j in range(length):
+                if i != j:
+                    if (weights[i]+weights[j]) not in d:
+                        d[weights[i]+weights[j]] = [i, j]
+                    elif limit in d:
+                        return d[limit]
+                    else:
+                        pass
+    
+    x = inner(weights, length, limit)
+
+    if length < 2:
+        return None
+    elif x[0] < x[1]:
+        x.reverse()
+    
+    return x
+
+
+
 
 print('\n\n', iterations, '\n\n')
 print(get_indices_of_item_weights([9], 1, 9))
@@ -14,23 +44,17 @@ iterations += 1
 print('\n\n', iterations, '\n\n')
 print(get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7))
 
-# x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-# y = 14
+# d = {}
 
-# def test(x, y):
-#     z = None
-#     count = 0
-#     for i in range(len(x)):
-#         for j in range(len(x)):
-#             count += 1
-#             print('Count:', count)
-#             z = x[i] + x[j]
-#             if y == z:
-#                 return i, j            
+# def inner(weights, length, limit):
+#     for i in range(length):
+#         for j in range(length):
+#             if i != j:
+#                 if (weights[i]+weights[j]) not in d:
+#                     d[weights[i]+weights[j]] = [i, j]
+#                 elif limit in d:
+#                     return d[limit]
+#                 else:
+#                     pass
 
-# i, j = test(x, y)
-
-# if x[i] < x[j]:
-#     return [x[j], x[i]]
-# else:
-#     return x[i], x[j]
+# print(inner([12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7))
